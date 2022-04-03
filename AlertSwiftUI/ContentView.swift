c303ff417e624c71c45a494eacdb4184faf2c659
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-04-03 14:06:07 +0300

    Initialized extension for alert

--- AlertSwiftUI/ContentView.swift
+++ AlertSwiftUI/ContentView.swift
@@ -19,3 +19,25 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+extension View {
+    func alertTF(title: String, message: String, hintText: String, primaryTitle: String, secondaryTitle: String, primatyAction: @escaping (String)->(), secondaryAction: @escaping ()->()) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addTextField { field in
+            field.placeholder = hintText
+        }
+        
+        alert.addAction(.init(title: secondaryTitle, style: .cancel, handler: { _ in secondaryAction()
+        }))
+        
+        alert.addAction(.init(title: primaryTitle, style: .cancel, handler: { _ in
+            if let text = alert.textFields?[0].text {
+                primatyAction(text)
+            }
+            else {
+                primaryAction()
+            }
+        }))
+    }
+}
