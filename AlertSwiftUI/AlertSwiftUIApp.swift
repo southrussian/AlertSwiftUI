208e15d10aae9788828f086a9a5a86e1fe507a37
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-04-03 13:44:20 +0300

    Initial Commit

---
+++ AlertSwiftUI/AlertSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertSwiftUIApp.swift
+//  AlertSwiftUI
+//
+//  Created by Danil Peregorodiev on 03.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AlertSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlertSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlertSwiftUI
+//
+//  Created by Danil Peregorodiev on 03.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
