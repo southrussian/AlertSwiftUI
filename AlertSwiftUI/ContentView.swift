b78a191f2995e00d031a2c958b20618eaf58f8b1
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-04-03 14:24:50 +0300

    Calling alertTF in ContentView by tapping the button

--- AlertSwiftUI/ContentView.swift
+++ AlertSwiftUI/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Call an Alert") {
+            alertTF(title: "Login", message: "Please enter your password for @south.russian", hintText: "123456", primaryTitle: "Login", secondaryTitle: "Cancel") { text in
+                print(text)
+            } secondaryAction: {
+                print("Cancelled")
+            }
+        }
     }
 }
 
@@ -31,7 +36,7 @@ extension View {
         alert.addAction(.init(title: secondaryTitle, style: .cancel, handler: { _ in secondaryAction()
         }))
         
-        alert.addAction(.init(title: primaryTitle, style: .cancel, handler: { _ in
+        alert.addAction(.init(title: primaryTitle, style: .default, handler: { _ in
             if let text = alert.textFields?[0].text {
                 primaryAction(text)
             }
@@ -39,6 +44,8 @@ extension View {
                 primaryAction("")
             }
         }))
+        
+        rootController().present(alert, animated: true, completion: nil)
     }
     func rootController()->UIViewController {
         guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
