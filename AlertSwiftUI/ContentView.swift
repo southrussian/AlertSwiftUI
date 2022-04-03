c2119d56d8f22fa836548273dd8db1cf72a2ae4c
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-04-03 14:11:59 +0300

    Added rootController function

--- AlertSwiftUI/ContentView.swift
+++ AlertSwiftUI/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 extension View {
-    func alertTF(title: String, message: String, hintText: String, primaryTitle: String, secondaryTitle: String, primatyAction: @escaping (String)->(), secondaryAction: @escaping ()->()) {
+    func alertTF(title: String, message: String, hintText: String, primaryTitle: String, secondaryTitle: String, primaryAction: @escaping (String)->(), secondaryAction: @escaping ()->()) {
         
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addTextField { field in
@@ -33,11 +33,20 @@ extension View {
         
         alert.addAction(.init(title: primaryTitle, style: .cancel, handler: { _ in
             if let text = alert.textFields?[0].text {
-                primatyAction(text)
+                primaryAction(text)
             }
             else {
-                primaryAction()
+                primaryAction("")
             }
         }))
+    }
+    func rootController()->UIViewController {
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return .init()
+        }
+        guard let root = screen.windows.first?.rootViewController else {
+            return .init()
+        }
+        return root
     }
 }
